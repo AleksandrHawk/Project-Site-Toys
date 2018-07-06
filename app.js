6b25bea51b9643b391b27eb01d4a618d62fef9e3
AleksandrHawk <publichawk@gmail.ru>
2018-07-07 01:42:27 +0300

    details toys

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+var toysRouter = express.Router();
+
 //установка механизма представления handlebars
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
@@ -15,6 +17,15 @@ app.use(express.static(__dirname + '/public'));
 app.use("/css", express.static(__dirname + '/css'));
 app.use("/images", express.static(__dirname + '/images'));
 
+//определяем маршрут с подмаршрутами
+toysRouter.route("/:toysName").get(function(req, res){
+    var tName = req.params.toysName;
+    res.render(tName);
+});
+
+
+
+app.use("/images", toysRouter);
 
 app.get('/', function(req, res){
     res.render('home');
